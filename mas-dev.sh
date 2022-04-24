b187ed016bc8437870f57a3c91285ba4f1fc8e72
purocean <purocean@gmail.com>
2022-04-24 14:43:07 +0800

    upd: update script

--- mas-dev.sh
+++ mas-dev.sh
@@ -6,5 +6,5 @@ sed -i '' 's/"mas"/"mas-dev"/' electron-builder.json
 yarn build:main
 yarn electron-builder --mac --arm64
 codesign -d --entitlements :- out/mas-dev-arm64/Yank\ Note.app/ | sed 's#<key>com.apple.developer.team-identifier</key><string>AGB983TWRL</string>##' > tmp.plist
-codesign -s '0AC36CCF8CD84E53418F089DA7810389794EAA4B' -f --entitlements ./tmp.plist ./out/mas-dev-arm64/Yank\ Note.app/
+codesign -s 'EAE127EFC2E3BC7FE3254B782450F23B5C8A9536' -f --entitlements ./tmp.plist ./out/mas-dev-arm64/Yank\ Note.app/
 rm tmp.plist
